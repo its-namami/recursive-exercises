330d4eae87c2ac19446ae9f8e87d837a4cacaea2
its-namami <164639260+its-namami@users.noreply.github.com>
2025-11-11 20:40:02 +0100

    feat: fix dup vs clone for frozen array, add recursive fibonacci

--- fibonacci/main.rb
+++ fibonacci/main.rb
@@ -6,8 +6,7 @@ def fibs(size)
   return STARTING_SEQUENCE.take(size) if size <= STARTING_SEQUENCE.size
 
   index = STARTING_SEQUENCE.size
-  fibonacci = STARTING_SEQUENCE
-
+  fibonacci = STARTING_SEQUENCE.dup
   while index < size
     index += 1
 
@@ -17,4 +16,15 @@ def fibs(size)
   fibonacci
 end
 
+def fibs_rec(size, fibonacci = STARTING_SEQUENCE.dup)
+  return STARTING_SEQUENCE.take(size) if size <= STARTING_SEQUENCE.size
+
+  return fibs_rec(size, fibonacci << fibonacci.last(2).sum) unless fibonacci.size >= size
+
+  fibonacci
+end
+
+p 'Iterative: '
 p fibs(8)
+p 'Recursive: '
+p fibs_rec(8)

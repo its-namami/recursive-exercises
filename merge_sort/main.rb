874de9e2368df84526fc443a63d45984a6a4dfc3
its-namami <164639260+its-namami@users.noreply.github.com>
2025-11-12 18:06:37 +0100

    feat: add merge sorted arrays method

--- merge_sort/main.rb
+++ merge_sort/main.rb
@@ -12,6 +12,19 @@ def sort_bin(array)
   array[0] > array[1] ? [array[1], array[0]] : array
 end
 
-def merge_arrays(one, two); end
+def merge_arrays(one, two, merged = [], index_one = 0, index_two = 0)
+  return merged + two[index_two..] if index_one == one.size
+  return merged + one[index_one..] if index_two == two.size
 
-def merge_sort; end
+  indexes = case one[index_one] < two[index_two]
+            when true then [index_one + 1, index_two]
+            else [index_one, index_two + 1]
+            end
+
+  smaller_item = [one[index_one], two[index_two]].min
+
+  merge_arrays(one, two, merged + [smaller_item], *indexes)
+end
+
+def merge_sort
+end

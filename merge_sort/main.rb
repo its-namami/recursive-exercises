ba04ebce35ad524a8aba32b347f2744edc23ce2b
its-namami <164639260+its-namami@users.noreply.github.com>
2025-11-14 18:03:24 +0100

    refactor: implement recursive merge sert v1.0.0

--- merge_sort/main.rb
+++ merge_sort/main.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
-require 'debug'
-
-# Probably don't need it since I don't even need to recursively div
-# def divide(array)
-#   return array if array.size == 1
-#
-#   halflen = array.size / 2
-#
-#   [array.take(halflen), array.drop(halflen)]
-# end
-#
-# Probably don't need this cuz I use the min method
-# def sort_bin(array)
-#   return array unless array.length == 2
-#
-#   array[0] > array[1] ? [array[1], array[0]] : array
-# end
+# divides array into two and returns both
+def divide(array)
+  halflen = array.size / 2
+  [array.take(halflen), array.drop(halflen)]
+end
 
 # merges two different arrays into a single one
 def merge_arrays(one, two, merged = [], index_one = 0, index_two = 0)
@@ -33,29 +21,14 @@ def merge_arrays(one, two, merged = [], index_one = 0, index_two = 0)
   merge_arrays(one, two, merged + [smaller_item], *indexes)
 end
 
-# init: needs sorted pairs array, assumes it now only needs to merge only
-def merge_sort(arrays); end
+def merge_sort(array)
+  return array if array.size == 1
 
-unsorted = [12, 98, 73, 46, 10, 28, 37, 49, 276, 63, 81, 29].each_slice(1).to_a.freeze
+  left, right = divide(array)
 
-temp1 = unsorted.dup
-temp2 = []
-
-until temp1.size == 1
-  j = 0
-
-  while j < temp1.size
-    if temp1[j + 1].nil?
-      temp2[-1] = merge_arrays(temp2[-1], temp1[j])
-      break
-    else
-      temp2 << merge_arrays(temp1[j], temp1[j + 1])
-      j += 2
-    end
-  end
-
-  temp1 = temp2.dup
-  temp2 = []
+  merge_arrays(merge_sort(left), merge_sort(right))
 end
 
-p temp1
+large_array = [85, 42, 19, 73, 56, 12, 99, 34, 61, 27]
+
+p merge_sort(large_array)
